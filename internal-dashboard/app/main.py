113666ab1fe639ddd12a8c6223ea9de4898d3acc
Stanislav Bzot <admin@MacBook-Pro-Stanislav.local>
2026-07-03 19:29:00 +0300

    horoshop_test_task

---
+++ internal-dashboard/app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import os
+
+ENV = os.environ["APP_ENV"]
+app = Flask(__name__)
+
+
+@app.route("/api/")
+def index():
+    return jsonify({"message": "Hello", "env": ENV})
+
+
+@app.route("/api/health")
+def health():
+    return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
